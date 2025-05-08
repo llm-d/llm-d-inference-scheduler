fb94a4505a8475f4077d49ecd97cfcbb2ce40279
Etai Lev Ran <elevran@gmail.com>
2025-05-08 09:35:24 +0300

    Update pkg/scheduling/plugins/scorer/session_affinity.go
    reference issue

--- pkg/scheduling/plugins/scorer/session_affinity.go
+++ pkg/scheduling/plugins/scorer/session_affinity.go
@@ -61,6 +61,7 @@ func (s *SessionAffinity) Score(ctx *types.SchedulingContext, pods []types.Pod) 
 // PostResponse sets the session header on the response sent to the client
 // TODO: this should be using a cookie and ensure not overriding any other
 // cookie values if present.
+// Tracked in https://github.com/neuralmagic/llm-d-inference-scheduler/issues/28
 func (s *SessionAffinity) PostResponse(ctx *types.SchedulingContext, pod types.Pod) {
 	ctx.MutatedHeaders[sessionTokenHeader] = base64.StdEncoding.EncodeToString([]byte(pod.GetPod().NamespacedName.String()))
 }
