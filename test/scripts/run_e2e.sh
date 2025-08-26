3798c53f86c27a5955ef24c179529fcd8e89bbd1
Shmuel Kallner <kallner@il.ibm.com>
2025-08-26 13:02:17 +0300

    Enable specifying tag for all used images, pulling if necessary
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- test/scripts/run_e2e.sh
+++ test/scripts/run_e2e.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
+# Set a default EPP_TAG if not provided
+export EPP_TAG="${EPP_TAG:-dev}"
+
 # Set a default VLLM_SIMULATOR_TAG if not provided
 export VLLM_SIMULATOR_TAG="${VLLM_SIMULATOR_TAG:-v0.4.0}"
 
 # Set the default routing side car image tag
 export ROUTING_SIDECAR_TAG="${ROUTING_SIDECAR_TAG:-v0.2.0}"
 
-if [[ "${VLLM_SIMULATOR_TAG}" != "dev" ]]; then
+SIMTAG=$(docker images | grep ghcr.io/llm-d/llm-d-inference-sim | awk '{print $2}' | grep ${VLLM_SIMULATOR_TAG})
+if [[ "${SIMTAG}" != "${VLLM_SIMULATOR_TAG}" ]]; then
   docker pull ghcr.io/llm-d/llm-d-inference-sim:${VLLM_SIMULATOR_TAG}
   if [[ $? != 0 ]]; then
     echo "Failed to pull ghcr.io/llm-d/llm-d-inference-sim:${VLLM_SIMULATOR_TAG}"
     exit 1
   fi
-else
-  SIMTAG=$(docker images | grep ghcr.io/llm-d/llm-d-inference-sim | awk '{print $2}' | grep dev)
-  if [[ "${SIMTAG}" != "dev" ]]; then
-    echo "Missing image ghcr.io/llm-d/llm-d-inference-sim:dev"
+fi
+
+EPPTAG=$(docker images | grep ghcr.io/llm-d/llm-d-inference-scheduler | awk '{print $2}' | grep ${EPP_TAG})
+if [[ "${EPPTAG}" != "${EPP_TAG}" ]]; then
+  docker pull ghcr.io/llm-d/llm-d-inference-scheduler:${EPP_TAG}
+  if [[ $? != 0 ]]; then
+    echo "Failed to pull ghcr.io/llm-d/llm-d-inference-scheduler:${EPP_TAG}"
     exit 1
   fi
 fi
 
-EPPTAG=$(docker images | grep ghcr.io/llm-d/llm-d-inference-scheduler | awk '{print $2}' | grep dev)
-if [[ "${EPPTAG}" != "dev" ]]; then
-  echo "Missing image ghcr.io/llm-d/llm-d-inference-scheduler:dev"
-  exit 1
+SIDECARTAG=$(docker images | grep ghcr.io/llm-d/llm-d-routing-sidecar | awk '{print $2}' | grep ${ROUTING_SIDECAR_TAG})
+if [[ "${SIDECARTAG}" != "${ROUTING_SIDECAR_TAG}" ]]; then
+  docker pull ghcr.io/llm-d/llm-d-routing-sidecar:${ROUTING_SIDECAR_TAG}
+  if [[ $? != 0 ]]; then
+    echo "Failed to pull ghcr.io/llm-d/llm-d-routing-sidecar:${ROUTING_SIDECAR_TAG}"
+    exit 1
+  fi
 fi
-
-docker pull ghcr.io/llm-d/llm-d-routing-sidecar:${ROUTING_SIDECAR_TAG}
 
 echo "Running end to end tests"
 
