8b70495d8906febc25134defeab00a7facc2bf70
Revital Sur <eres@il.ibm.com>
2026-03-15 10:33:29 +0200

    Rename files and structs.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- pkg/plugins/profile/always_encode_decider.go
+++ pkg/plugins/profile/always_encode_decider.go
@@ -14,7 +14,7 @@ const (
 )
 
 // compile-time type assertion
-var _ epdDeciderPlugin = &AlwaysEncodeDecider{}
+var _ e_pdDeciderPlugin = &AlwaysEncodeDecider{}
 
 // AlwaysEncodeDecider is an EP decider plugin which always decides to encode.
 type AlwaysEncodeDecider struct {
