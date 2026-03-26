be15cdad6d9988280a1267e6fc36e8619ec6a87a
roytman <roytman@il.ibm.com>
2026-03-26 14:27:28 +0200

    fix lint errors
    Signed-off-by: roytman <roytman@il.ibm.com>

--- test/e2e/utils_test.go
+++ test/e2e/utils_test.go
@@ -41,7 +41,7 @@ func waitForEPPToDiscoverPods(modelName string) {
 		if err != nil {
 			return false
 		}
-		defer resp.Body.Close()
+		defer func() { _ = resp.Body.Close() }()
 		if resp.StatusCode != http.StatusServiceUnavailable {
 			return true
 		}
